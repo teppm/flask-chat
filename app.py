5d306a0ec169341399cac3743266a71a286601c0
unknown <madis.tepp@gmail.com>
2020-02-16 18:27:33 +0100

    chat app

--- app.py
+++ app.py
@@ -1,27 +1,44 @@
 import os 
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+message = []
+
+def add_messages(username, message):
+    """Add messages to messages list """ 
+
+    message.append("{}:{}".format(username, message))
+
+
+def get_all_message():
+    """Get all message and separate with br"""
+
+    return "<br>".join(message)
+
 
 @app.route('/')
-
 
 def index():
 
     """main page with instructions"""
     return "To send message use /USERNAME/MESSAGE"
 
-app.route('/username')
+app.route('/<username>')
 
 def user(username):
-    return "Hi" + username
+    """Display chat messages""" 
+
+    return "<h1>Welcom</h1>, {0}</h1>,{1}".format(username, get_all_message())
 
 app.route('/<username>/<message>')
 
+
 def send_msg(username, message):
     """create a new message and redirect back to chat page""" 
-    return "{0}:{1}".format(username, message)
+
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 
-app.run(host=os.getenv('IP'), port=(os.getenv('PORT')), debug=True)
+app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
