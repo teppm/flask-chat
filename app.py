38bd8b90196765aa76eab4f4eb88b69c3c6d5c65
unknown <madis.tepp@gmail.com>
2020-02-16 13:00:17 +0100

    routing started

--- app.py
+++ app.py
@@ -1,2 +1,26 @@
 import os 
-import flask from Flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+
+def index():
+
+    """main page with instructions"""
+    return "To send message use /USERNAME/MESSAGE"
+
+app.route('/username')
+
+def user(username):
+    return "Hi" + username
+
+app.route('/<username>/<message>')
+
+def send_msg(username, message):
+    return "{0}:{1}".format(username, message)
+
+
+
+app.run(host=os.getenv('IP'), port=(os.getenv('PORT')), debug=True)
