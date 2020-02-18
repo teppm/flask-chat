3b032831a3d435c6b33ccf65e2350c79a623d582
unknown <madis.tepp@gmail.com>
2020-02-18 20:34:46 +0100

    fixed @app

--- app.py
+++ app.py
@@ -10,14 +10,13 @@ app.secret_key = "1secret2key"
 messages = []
 
 def add_messages(username, message):
-    """Add messages to messages list """ 
+    """Add messages to ´messages´ list """ 
     now = datetime.now().strftime("%H:%M:%S")
     messages.append("({}){}:{}".format(now, username, message))
 
 
-def get_all_message():
+def get_all_messages():
     """Get all message and separate with br"""
-
     return "<br>".join(messages)
 
 
@@ -39,16 +38,17 @@ app.route("/<username>")
 def user(username):
     """Display chat messages""" 
 
-    return "<h1>Welcome!</h1>, {0}</h1>,{1}".format(username, get_all_message())
-
-app.route("/<username>/<message>")
+    return "<h1>Welcome!, {0}</h1>,{1}".format(username, get_all_messages())
 
 
-def send_msg(username, message):
+@app.route("/<username>/<message>")
+
+def send_message(username, message):
     """create a new message and redirect back to chat page""" 
-
     add_messages(username, message)
     return redirect("/" + username)
+
+    
 
 
 
